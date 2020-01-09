863368c529ac93a65c6839cd3d6224eeb475a0c6
RyDeem <58887105+RyDeem@users.noreply.github.com>
2020-01-09 09:31:25 +0200

    Update Backtrack.cpp
    Reverted bcuz im a doodoo

--- Osiris/Hacks/Backtrack.cpp
+++ Osiris/Hacks/Backtrack.cpp
@@ -37,11 +37,11 @@ void Backtrack::update(FrameStage stage) noexcept
             records[i].push_front(record);
 
 			float latency = 0.0f;
-			if (config.backtrack.pingBased) {
-				if (auto networkChannel = interfaces.engine->getNetworkChannel(); networkChannel && networkChannel->getLatency(0) > 0.0f)
-					latency = networkChannel->getLatency(0);
-				latency = static_cast<int>(latency * 1000);
-			};
+			if (auto networkChannel = interfaces.engine->getNetworkChannel(); networkChannel && networkChannel->getLatency(0) > 0.0f)
+				latency = networkChannel->getLatency(0);
+			if (config.backtrack.pingBased)
+				config.backtrack.timeLimit = static_cast<int>(latency * 1000);
+
 
             while (records[i].size() > 3 && records[i].size() > static_cast<size_t>(timeToTicks(static_cast<float>(config.backtrack.timeLimit) / 1000.f)))
                 records[i].pop_back();
