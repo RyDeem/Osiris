cb7a67589598c16b4b3a6f54cb7c9af2cc3b75cc
RyDeem <58887105+RyDeem@users.noreply.github.com>
2020-01-11 00:33:54 +0200

    Update GUI.cpp
    typo

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -857,7 +857,7 @@ void GUI::renderVisualsWindow() noexcept
 		ImGui::Checkbox("Hit Damage", &config.visuals.hitMarkerDamageIndicator);
 		if (config.visuals.hitMarkerDamageIndicator) {
 			ImGui::SameLine();
-			ImGui::Checkbox("Customize Hitmarker", &config.visuals.hitMarkerDamageIndicatorCustomize);
+			ImGui::Checkbox("Customize HitDamage", &config.visuals.hitMarkerDamageIndicatorCustomize);
 		};
 		ImGui::PushID(14);
 		ImGui::SliderFloat("", &config.visuals.hitMarkerTime, 0.01f, 1.0f, "Hit marker time: %.2fs");
