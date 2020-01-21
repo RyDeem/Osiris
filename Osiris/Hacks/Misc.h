5c9debbd9da60015e7c01ba7b68828f8b20bff53
RyDeem <58887105+RyDeem@users.noreply.github.com>
2020-01-21 18:41:34 +0200

    Added bomb damage indicator #1003Added bomb damage indicator from  #1003

--- Osiris/Hacks/Misc.h
+++ Osiris/Hacks/Misc.h
@@ -22,6 +22,7 @@ namespace Misc
     void watermark() noexcept;
     void prepareRevolver(UserCmd*) noexcept;
     void fastPlant(UserCmd*) noexcept;
+    void drawBombDamage() noexcept;
     void drawBombTimer() noexcept;
     void stealNames() noexcept;
     void disablePanoramablur() noexcept;
